14f3ddab3cd054592206eb17c2fd0d8ad181152f
Diego Solis <disc3110@gmail.com>
2022-02-15 17:39:22 -0600

    query and update animals table

--- data.sql
+++ data.sql
@@ -50,3 +50,55 @@ INSERT INTO animals (
     , TRUE
     , 11
   );
+
+  INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Charmander', 'feb/8/20', 0, FALSE, -11);
+
+
+   INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Plantmon', 'nov/15/22', 2, TRUE, -5.7);
+
+   INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Squirtle', 'apr/2/93', 3, FALSE, -12.13);
+
+
+   INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Angemon', 'jun/12/2005', 1, TRUE, -45);
+
+
+   INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Boarmon', 'jun/07/2005', 7, TRUE, 20.4);
+
+
+   INSERT INTO animals (
+       name
+       , date_of_birth
+       , escape_attempts
+       , neutered
+       , weight_kg
+   ) VALUES ('Blossom', 'oct/13/1998', 3, TRUE, 17); 
--- schema.sql
+++ schema.sql
@@ -8,3 +8,6 @@ CREATE DATABASE vet_clinic;
       neutered BOOLEAN,
       weight_kg DECIMAL
   ); 
+
+  ALTER TABLE animals
+         ADD COLUMN species VARCHAR(100); 
